674049d02678394df21ca4940dd4096aaf706d4e
youngkwan-Ji <youngkwanji@gmail.com>
2022-06-17 18:34:13 +0900

    feat: add insert, select func

--- hbaseService.js
+++ hbaseService.js
@@ -1,46 +1,58 @@
 const {client} = require('services/hbase-client/hbaseConnecter')
+const {resolve} = require("path");
 
 
 function initTable(){
     client
         .table("tb_kline")
         .create("cf_origin", (err, success) => {
-            if (success){
-                client
-                    .table("tb_kline")
-                    .row("BTCUSDT")
-                    .put("cf_origin:test","1234", (err, success)=>{
-                        if (success){
-                            client
-                                .table('tb_kline' )
-                                .row('BTCUSDT')
-                                .get('cf_origin', function(err, [cell]){
-                                    // Validate the result
-                                    console.log(cell)
-                                })
-                        }else{
-                            console.log(err)
-                        }
-                    })
-            }else{
-                console.log(err)
-            }
+            success ? console.log("initTable : " + success) : console.log(err)
         })
 }
 
-function createTable(data){
+function deleteTable(){
     client
-        .table(data["table"])
+        .table("tb_kline")
+        .delete((err,success)=>{
+            success ? console.log("deleteTable : " + success) : console.log(err)
+        })
 }
 //
-async function insertKlineData(data) {
-    client
-        .table('tb_kline')
-        .row("BTCUSDT")
-        .put("cf_origin:test", data, async (err,success)=>{
-            await new Promise((resolve, reject) => {})
-            return
-        })
+async function insertKline(data) {
+    return new Promise((resolve,reject) => {
+        client
+            .table('tb_kline')
+            .row("BTCUSDT_" + new Date())
+            .put("cf_origin:test", data, (err, success) => {
+                success ? resolve(success) : reject(err)
+            })
+    })
 }
 
-module.exports = {initTable,insertKlineData}
+
+function selectKline(data) {
+    const scanner = client
+        .table("tb_kline")
+        .scan({
+            startRow: "BTCUSDT"
+        })
+    const rows = []
+
+    scanner.on('readable', function(){
+        while(chunk = scanner.read()){
+            console.log('read');
+            rows.push(chunk);
+            console.log(rows)
+        }
+    });
+    scanner.on('error', function(err){
+        console.log(err);
+    });
+    scanner.on('end', function(){
+        console.log('end');
+        console.log(rows);
+    });
+
+}
+
+module.exports = {initTable,insertKline,selectKline}
